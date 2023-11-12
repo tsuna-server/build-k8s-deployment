627947178858b3d7925fac2731a16d46a54bafac
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2023-11-12 20:29:14 +0900

    Added a script get_own_ip.sh

---
+++ roles/k8s_masters/files/get_own_ip.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+log_err() { echo "$(date) - ERROR: $1" >&2; }
+
+main() {
+    local ip
+    ip="$(getent hosts "$(hostname)" | cut -d ' ' -f 1)"
+
+    if [ -z "${ip}" ]; then
+        log_err "Failed to get IP address of host \"getent hosts \\\"\$(getent hosts \\\"\$(hostname)\\\" | cut -d ' ' -f 1)\\\"\". Its value was empty."
+        return 1
+    fi
+
+    if [[ ! "${ip}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        log_err "Failed to get IP address of host \"getent hosts \\\"\$(getent hosts \\\"\$(hostname)\\\" | cut -d ' ' -f 1)\\\"\". Its value was \"${ip}\""
+        return 1
+    fi
+
+    echo "${ip}"
+
+    return 0
+}
+
+main "$@"
